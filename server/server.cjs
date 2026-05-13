468d833c0aef02956120014b40b013d07e5dc4bf
VatsalAggarwal31 <vatsalaggarwal31@gmail.com>
2026-05-13 23:39:39 +0530

    Express 5 regex routing fix

--- server/server.cjs
+++ server/server.cjs
@@ -411,7 +411,7 @@ const distPath = path.join(__dirname, '../dist');
 app.use(express.static(distPath));
 
 // Handle React routing, return all requests to React app
-app.get('/*', (req, res) => {
+app.get('/(.*)', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
